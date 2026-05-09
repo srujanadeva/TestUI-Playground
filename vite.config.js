7ecd99060f4c25b645f45430507f890d437fb109
Srujana Deva <srujohn@gmail.com>
2026-05-09 17:01:57 +0530

    Resolved critical CVEs

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: '0.0.0.0',
+    host: 'localhost',
     https: {
       key: fs.readFileSync('./certs/key.pem'),
       cert: fs.readFileSync('./certs/cert.pem'),
