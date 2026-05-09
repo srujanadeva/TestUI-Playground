f56c4148b952b7e7068591928ce783ffdc085276
Srujana Deva <srujohn@gmail.com>
2026-05-09 16:17:11 +0530

    Enabled SSL with self sign cert

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, host: '0.0.0.0' }
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    https: {
+      key: fs.readFileSync('./certs/key.pem'),
+      cert: fs.readFileSync('./certs/cert.pem'),
+    }
+  }
 })
